0d9f5e6701099453373d4de7ae3b0a8a2ce0e555
guillermodep <gdeprati@santandertecnologia.com.ar>
2020-07-29 21:34:10 -0300

    tittle

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "<h1><center>MultiClusterManagement Santander Tecnologia</h1></center>"
 
 if __name__ == "__main__":
     application.run()
