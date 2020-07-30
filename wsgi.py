071fa63151379f4803061766b9886dec70a168d5
guillermodep <gdeprati@santandertecnologia.com.ar>
2020-07-29 22:03:15 -0300

    fix app variable

--- wsgi.py
+++ wsgi.py
@@ -6,20 +6,20 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     return "<h1><center>MultiClusterManagement Santander Tecnologia</h1></center>"
-
-@app.route("/index.html")
+"""
+@application.route("/index.html")
 def index():
     return render_template('index.html')
 
-@app.route("/login/", methods=["POST"])
+@application.route("/login/", methods=["POST"])
 def login():
     if request.method == "POST":
         username = request.form.get("username")
         return redirect(url_for('user', username=username))
     return redirect(url_for('index'))
 
-@app.route("/user/")
-@app.route("/user/<username>")
+@application.route("/user/")
+@application.route("/user/<username>")
 def user(username=None):
     if not username:
         return redirect(url_for('index'))
@@ -27,3 +27,4 @@ def user(username=None):
 
 if __name__ == "__main__":
     application.run()
+"""
